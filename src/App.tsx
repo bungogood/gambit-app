c6e11e8c65add791c00eb333d41c78fa21b3eecd
Jonathan Good <github@bungogood.com>
2023-05-16 19:55:18 +0100

    Adding BLE

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import '@ionic/react/css/core.css';
 import { setupIonicReact } from '@ionic/react';
 import Board from './board';
+import BLETest from './ble';
 
 setupIonicReact();
 
 function App() {
   return (
     <>
+      <BLETest/>
       <Board/>
     </>
   )
